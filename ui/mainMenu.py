b076e52abdda2fd19c599f1968841a1d587ebe04
zhangLily2020 <zhangyuzhelily@gmail.com>
2025-01-18 12:28:53 -0800

    mainMenu

---
+++ ui/mainMenu.py
@@ -0,0 +1,19 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
